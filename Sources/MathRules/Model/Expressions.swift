9b14b6cf4ae90fa2589e82314d3ad27a49966152
Michel Tilman <mtilman@acm.org>
2020-12-21 12:14:42 +0100

    Cleaned up, refactored and documented code. WIP. No unit tests yet.

--- Sources/MathRules/Model/Expressions.swift
+++ Sources/MathRules/Model/Expressions.swift
@@ -7,28 +7,55 @@
 //  Licensed under Apache License v2.0.
 //
 
+/**
+ 'Abstract' superclass representing expressions consisting of elementary operations.
+ These building blocks may be combined in more complex structures.
+ Would have preferred protocol and structs / recursive enumes, but both have some restrictions
+ needlessly complicating the code.
+ */
 public class Expression<T> {
     
-    func eval(in context: Context<T>) -> T {
+    // MARK: Evaluating
+
+    /// Evaluates the receiver in given context and answers the result.
+    /// Should be abstract method.
+    public func eval(in context: Context<T>) -> T {
         fatalError("Subclass responsibility")
     }
     
 }
 
+
+/**
+ Evaluation context for expressions.
+ Contains dictionary of user-defined functions and parameters for the current expression
+ to be evaluated.
+ */
 public struct Context<T> {
     
+    // MARK: Stored properties
+
+    // User-defined functions.
     let functions: [String: Function<T>]
     
+    // Parameters needed by expression being evaluated.
     let parameters: [T]
     
+    // MARK: Accessing by subscripting
+
+    // Answers the function with given name or nil if not found.
     subscript(function name: String) -> Function<T>? {
         functions[name]
     }
     
+    // Answers the parameter at given index or nil if not found.
     subscript(parameter index: Int) -> T? {
         index >= 0 && index < parameters.count ? parameters[index] : nil
     }
     
+    // MARK: Copying
+
+    // Answers a copy of the context with the parameters replaced by given list.
     func withParameters(_ parameters: [T]) -> Self {
         Self(functions: functions, parameters: parameters)
     }
@@ -36,12 +63,20 @@ public struct Context<T> {
 }
 
 
+/**
+ Represents user-defined functions.
+ */
 public struct Function<T> {
     
+    // MARK: Stored properties
+
     let name: String
     
     let expression: Expression<T>
     
+    // MARK: Evaluating
+
+    // Evaluates the receiver in given context and answers the result.
     func eval(in context: Context<T>) -> T {
         expression.eval(in: context)
     }
--- Sources/MathRules/Model/Operations.swift
+++ Sources/MathRules/Model/Operations.swift
@@ -8,33 +8,122 @@
 //
 
 /**
- 'Abstract' superclass representing state and initialization of operations involving one operand.
+ Utility types for operations.
  */
-public class UnaryOperation<T, A>: Expression<T> {
+public enum Operation<T> {
     
-    /// The single operand.
-    public let operand: A
+    // MARK: Convenience type aliasses
     
-    /// Initializes the operand.
-    public init(_ operand: A) {
-        self.operand = operand
+    public typealias Context = MathRules.Context<T>
+    public typealias Expression = MathRules.Expression<T>
+
+    // MARK: Common implementation classes for operations
+    
+    /// 'Abstract' superclass representing state and initialization of operations involving one operand.
+    public class UnaryOperation<A>: Expression {
+        
+        /// The single operand.
+        public let operand: A
+        
+        /// Initializes the operand.
+        public init(_ operand: A) {
+            self.operand = operand
+        }
+        
     }
-    
+
+    /// 'Abstract' superclass representing state and initialization of operations involving two operands.
+    public class BinaryOperation<A, B>: Expression {
+        
+        /// Left- and right-hand-side operands.
+        public let lhs: A, rhs: B
+        
+        /// Initializes the left- and right-hand-side operands.
+        public init(_ lhs: A, _ rhs: B) {
+            self.lhs = lhs
+            self.rhs = rhs
+        }
+        
+    }
+
 }
 
 
 /**
- 'Abstract' superclass representing state and initialization of operations involving two operands.
+ Core operations.
  */
-public class BinaryOperation<T, A, B>: Expression<T> {
+extension Operation {
     
-    /// Left- and right-hand-side operands.
-    public let lhs: A, rhs: B
+    // MARK: Core operations
     
-    /// Initializes the left- and right-hand-side operands.
-    public init(_ lhs: A, _ rhs: B) {
-        self.lhs = lhs
-        self.rhs = rhs
+    /// Represents a function call, identified by a function name and a list of parameters.
+    /// Currently does not verify consistency of function and parameters.
+    public class FunctionCall: BinaryOperation<String, [Expression]> {
+        
+        /// Evaluates the function call in given context and answers the result.
+        /// Fails if the function does not exist in the context.
+        /// Parameters are evaluated eagerly.
+       public override func eval(in context: Context) -> T {
+            guard let function = context[function: lhs] else { fatalError("Unknown function") }
+        
+            return function.eval(in: context.withParameters(rhs.map { $0.eval(in: context) }))
+        }
+        
     }
     
+    /// Represents a constant value.
+    public class Literal: UnaryOperation<T> {
+        
+        /// Evaluates the literal call in given context and answers the literal.
+        public override func eval(in context: Context) -> T {
+            operand
+        }
+        
+    }
+    
+    /// References a parameter in a context.
+    public class Parameter: UnaryOperation<Int> {
+        
+        /// Looks up a parameter in the context.
+        /// Fails if the parameter cannot be found.
+        public override func eval(in context: Context) -> T {
+            guard let parameter = context[parameter: operand] else { fatalError("Unknown parameter") }
+        
+            return parameter
+        }
+
+    }
+    
+    // MARK: Shorthand functions to create core operations
+    
+    /// Answers a function call.
+    static func call(_ function: String, parameters: [Expression]) -> FunctionCall {
+        FunctionCall(function, parameters)
+    }
+
+    /// Answers a literal value.
+    static func cons(_ value: T) -> Literal {
+        Literal(value)
+    }
+
+    /// Answers a parameter reference.
+    static func param(_ index: Int) -> Parameter {
+        Parameter(index)
+    }
+
+}
+
+
+/**
+ Core operations for types conforming to LosslessStringConvertible.
+ */
+extension Operation where T: LosslessStringConvertible {
+    
+    // MARK: Creating literals from strings
+    
+    /// Answers a literal value for gven type, or nil if not valid.
+    static func const(_ string: String) -> Literal? {
+        T(string).map(Literal.init)
+    }
+
 }
